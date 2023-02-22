75ac3506faf81fc80a13778127acfd7e9e25c793
wodahS21 <yaelelyolo@gmail.com>
2023-02-22 09:23:32 -0700

    cambio del metodo random, se aplico para las figuras seleccionadas

--- u2_1/Form1.cs
+++ u2_1/Form1.cs
@@ -26,30 +26,104 @@ namespace u2_1
             SolidBrush brocha = new SolidBrush(Color.Black);
             if (cbLinea.Checked)
             {
-                papel.DrawLine(lapiz,10,10,100,10);
-                cbLinea.Checked = !cbLinea.Checked;
+                if (cbRandom.Checked)
+                {
+                    Random r = new Random();
+                    int n = r.Next(5);
+                    Random x1 = new Random();
+                    int x = x1.Next(280);
+                    Random y1 = new Random();
+                    int y = y1.Next(300);
+                    papel.DrawLine(lapiz, x, y, 100, 10);
+                    cbLinea.Checked = !cbLinea.Checked;
+                }
+                else
+                {
+                    papel.DrawLine(lapiz, 10, 10, 100, 10);
+                    cbLinea.Checked = !cbLinea.Checked;
+                }
+                 
             }
             if (cbRectangulo.Checked)
             {
-                papel.DrawRectangle(lapiz, 110, 10, 100, 50);
-                cbRectangulo.Checked = !cbRectangulo.Checked;
+                if (cbRandom.Checked)
+                {
+                    Random r = new Random();
+                    int n = r.Next(5);
+                    Random x1 = new Random();
+                    int x = x1.Next(280);
+                    Random y1 = new Random();
+                    int y = y1.Next(300);
+                    papel.DrawRectangle(lapiz, x, y, 100, 50);
+                    cbRectangulo.Checked = !cbRectangulo.Checked;
+                }
+                else
+                {
+                    papel.DrawRectangle(lapiz, 110, 10, 100, 50);
+                    cbRectangulo.Checked = !cbRectangulo.Checked;
+                }
+                
             }
             if (cbCuadrado.Checked)
             {
-                papel.DrawRectangle(lapiz, 220, 10, 50, 50);
-                cbCuadrado.Checked = !cbCuadrado.Checked;
+                if (cbRandom.Checked)
+                {
+                    Random r = new Random();
+                    int n = r.Next(5);
+                    Random x1 = new Random();
+                    int x = x1.Next(280);
+                    Random y1 = new Random();
+                    int y = y1.Next(300);
+                    papel.DrawRectangle(lapiz, x, y, 50, 50);
+                    cbCuadrado.Checked = !cbCuadrado.Checked;
+                }
+                else
+                {
+                    papel.DrawRectangle(lapiz, 220, 10, 50, 50);
+                    cbCuadrado.Checked = !cbCuadrado.Checked;
+                }
             }
             if (cbElipce.Checked)
             {
-                papel.DrawEllipse(lapiz, 280, 10, 100, 50);
-                cbElipce.Checked = !cbElipce.Checked;
+                if (cbRandom.Checked)
+                {
+                    Random r = new Random();
+                    int n = r.Next(5);
+                    Random x1 = new Random();
+                    int x = x1.Next(280);
+                    Random y1 = new Random();
+                    int y = y1.Next(300);
+                    papel.DrawEllipse(lapiz, x, y, 100, 50);
+                    cbElipce.Checked = !cbElipce.Checked;
+                }
+                else
+                {
+                    papel.DrawEllipse(lapiz, 280, 10, 100, 50);
+                    cbElipce.Checked = !cbElipce.Checked;
+                }
+               
             }
             if (cbElipcell.Checked) 
             {
-                papel.FillEllipse(brocha, 10, 70, 100, 50);
-                cbElipcell.Checked = !cbElipcell.Checked;
+                if (cbRandom.Checked)
+                {
+                    Random r = new Random();
+                    int n = r.Next(5);
+                    Random x1 = new Random();
+                    int x = x1.Next(280);
+                    Random y1 = new Random();
+                    int y = y1.Next(300);
+                    papel.FillEllipse(brocha, x,y, 100, 50);
+                    cbElipcell.Checked = !cbElipcell.Checked;
+                }
+                else
+                {
+                    papel.FillEllipse(brocha, 10, 70, 100, 50);
+                    cbElipcell.Checked = !cbElipcell.Checked;
+                }
+                
             }
-            if (cbRandom.Checked)
+           /* if (cbRandom.Checked)
             {
                 Random r = new Random();
                 int n= r.Next(5);
@@ -58,20 +132,20 @@ namespace u2_1
                 Random y1 = new Random();
                 int y = y1.Next(300);
 
-                switch (n)
-                {
-                    case 1:papel.DrawLine(lapiz, x, y, 100, 10); break;
-                    case 2: papel.DrawRectangle(lapiz, x, y, 100, 50); break;
-                    case 3: papel.DrawRectangle(lapiz, x, y, 50, 50); break;
-                    case 4: papel.DrawEllipse(lapiz, x, y, 100, 50); break;
-                    case 5: papel.FillEllipse(brocha, x, y, 100, 50); break;
+                 switch (n)
+                 {
+                     case 1:papel.DrawLine(lapiz, x, y, 100, 10); break;
+                     case 2: papel.DrawRectangle(lapiz, x, y, 100, 50); break;
+                     case 3: papel.DrawRectangle(lapiz, x, y, 50, 50); break;
+                     case 4: papel.DrawEllipse(lapiz, x, y, 100, 50); break;
+                     case 5: papel.FillEllipse(brocha, x, y, 100, 50); break;
 
-                }
+                 }
                 cbRandom.Checked = !cbRandom.Checked;
+             }*/
+
             }
-            
-        }
-        private void Form1_Load(object sender, EventArgs e)
+            private void Form1_Load(object sender, EventArgs e)
         {
 
         }
